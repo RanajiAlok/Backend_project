1face450cb05f70303c7739acf856a83a7046994
Alok Kumar Singh <86158900+RanajiAlok@users.noreply.github.com>
2025-01-29 16:20:28 +0530

    added gitignore

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed!!", err);
+  });
 
 /*import express from "express";
 const app = express()(async () => {
